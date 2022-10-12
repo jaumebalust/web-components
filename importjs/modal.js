1c86ececa5307907864f59bd42b35d116650d27a
Jimmy Balust <balust.jaume@gmail.com>
2022-10-12 09:24:12 +0200

    finished lesson 4. modal

--- importjs/modal.js
+++ importjs/modal.js
@@ -1,10 +1,13 @@
 class Modal extends HTMLElement {
     constructor() {
         super();
+        this.isOpen = false
         this.attachShadow({mode:'open'})
 
         this.shadowRoot.innerHTML = `
         <style>
+       
+        
             #backdrop{
                 position: fixed;
                 top:0;
@@ -12,6 +15,15 @@ class Modal extends HTMLElement {
                 width:100%;
                 height:100vh;
                 background-color: rgba(0,0,0,0.24);
+                z-index: 10;
+                opacity: 0;
+                pointer-events: none;
+               
+            }
+            :host([opened]) #backdrop,
+            :host([opened]) #modal{
+                opacity: 1;   
+                pointer-events: all;   
             }
             #modal{
                 position:fixed;
@@ -26,6 +38,9 @@ class Modal extends HTMLElement {
                 display: flex;
                 flex-direction: column;
                 justify-content: space-between;
+                opacity: 0;
+                pointer-events: none;
+                transition: all 0.3s ease-out;
             }
             #actions{
                 border-top: 1px solid lightgray;
@@ -46,23 +61,82 @@ class Modal extends HTMLElement {
             #main{
                 padding: 1rem;
             }
+            ::slotted(h1){
+                font-size: 20px;
+            }
         </style>
         <div id="backdrop"></div> 
         <div id="modal">
             <header>
-                <h1>Please Confirm</h1>
+                <slot name="title">Please confirm payment</slot>
+               
             </header>
             <section id="main">
                 <slot></slot>
             </section>
             <section id="actions">
-                <button>Cancel</button>
-                <button>Okay</button>
+                <button id="cancel">Cancel</button>
+                <button id="confirm">Okay</button>
             </section>
             
         </div> 
         `
 
+        const slots = this.shadowRoot.querySelectorAll('slot')
+        slots[1].addEventListener('slotchange',(event)=>{
+            console.log(event)
+        })
+
+        const cancelButton = this.shadowRoot.querySelector('#cancel')
+        const confirmButton = this.shadowRoot.querySelector('#confirm')
+        const backdrop = this.shadowRoot.querySelector('#backdrop')
+
+        backdrop.addEventListener('click',this._cancel.bind(this))
+        cancelButton.addEventListener('click',this._cancel.bind(this))
+        confirmButton.addEventListener('click',this._confirm.bind(this))
+
+    }
+
+    _cancel(){
+        this.hide()
+        const cancelEvent = new Event('cancel')
+        this.dispatchEvent(cancelEvent)
+    }
+    _confirm(){
+        this.hide()
+        const confirmEvent = new Event('confirm')
+        this.dispatchEvent(confirmEvent)
+    }
+
+
+    open(){
+        this.setAttribute('opened','')
+        this.isOpen = true
+    }
+    hide(){
+        if (this.hasAttribute('opened')){
+            this.removeAttribute('opened')
+        }
+        this.isOpen = false
+    }
+
+    attributeChangedCallback(name,oldValue,newValue){
+        if (oldValue === newValue){
+            return
+        }
+        if (name === 'opened'){
+            if (this.hasAttribute('opened')){
+                this.isOpen = true
+
+            } else {
+                this.isOpen = false
+            }
+        }
+
+    }
+
+    static get observedAttributes(){
+        return ['opened']
     }
 }
 
